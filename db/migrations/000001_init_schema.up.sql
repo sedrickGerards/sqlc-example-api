dac33c4df6ef49c95028278de0aef39bc4a2619d
sedrickGerards <mubangsedrick@gmail.com>
2025-04-10 13:53:35 +0100

    i had added to the threads table and linked it's ID  to the message table as a foeign key

--- db/migrations/000001_init_schema.up.sql
+++ db/migrations/000001_init_schema.up.sql
@@ -1,12 +1,25 @@
 
+-- creating the thread table 
 
+CREATE TABLE "thread" (
+
+    "id" VARCHAR(36) PRIMARY KEY DEFAULT gen_random_uuid()::varchar(36),
+    "name" TEXT NOT NULL,
+    "created_at" TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
 
 
 CREATE TABLE "message" (
+
   "id" VARCHAR(36) PRIMARY KEY DEFAULT gen_random_uuid()::varchar(36),
-  "thread" VARCHAR(36) NOT NULL,
+  "thread_id" VARCHAR(36) NOT NULL,                      -- thread id for the foreign key
   "sender" VARCHAR(100) NOT NULL,
   "content" TEXT NOT NULL,
-  "created_at" TIMESTAMP DEFAULT now()
+  "created_at" TIMESTAMP DEFAULT now(),
+
+  -- foreign key constraint to reference the thread table
+
+  FOREIGN KEY ("thread_id") REFERENCEs thread("id")
 );
+
 
